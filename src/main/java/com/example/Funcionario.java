e4d2ad50eefdcf1be3879b79237cf1cb5585c125
Pablo <pablomagalhes@gmail.com>
2024-06-19 17:47:54 -0300

    versao1

---
+++ src/main/java/com/example/Funcionario.java
@@ -0,0 +1,184 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Funcionario {
+
+    private int cdFuncionario;
+    private String nmFuncionario;
+    private LocalDate dtNascimento;
+    private double salario;
+    private String depart;
+    private String cargo;
+
+
+
+    public Funcionario() {
+        //TODO Auto-generated constructor stub
+    }
+
+    //CONSTRUTOR PADRÃO
+    public Funcionario (int cdFuncionario, String nmFuncionario,
+    LocalDate dtNascimento, double salario, String depart, String cargo) {
+
+
+        this.cdFuncionario = cdFuncionario;
+        this.nmFuncionario = nmFuncionario;
+        this.dtNascimento = dtNascimento;
+        this.salario = salario;
+        this.depart = depart;
+        this.cargo = cargo;
+
+    
+    }
+
+
+    
+
+
+    public  Funcionario criarFuncionario(Scanner s) {
+
+        System.out.println("Digite o código do Funcionário:");
+        int cdFuncionario = s.nextInt();
+        s.nextLine();
+
+        System.out.println("Digite o nome do Funcionário:");
+        String nmFuncionario = s.nextLine();
+
+        System.out.println("Digite o nome do Departamento:");
+        String depart = s.nextLine();
+
+        System.out.println("Digite o salário:");
+        double salario = s.nextDouble();
+        s.nextLine();
+
+        System.out.println("Digite a data de Nascimento (dd/MM/yyyy):");
+        String dataNascimentoStr = s.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dtNascimento = LocalDate.parse(dataNascimentoStr, formatter);
+
+        return new Funcionario(cdFuncionario, nmFuncionario, dtNascimento, salario, depart, "");
+    }
+
+   
+    public void salvarFuncionarioBD() {
+
+        String urlDB = "jdbc:postgresql://localhost:5432/postgres";
+        String userDB = "postgres";
+        String passwordDB = "7412369";
+
+
+        String sql = "INSERT INTO funcionarios (cdFuncionario, nmFuncionario, dtNascimento, salario, depart, cargo) " +
+        "VALUES (?, ?, ?, ?, ?, ?";
+
+        try (Connection conn = DriverManager.getConnection(urlDB, userDB, passwordDB);
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, cdFuncionario);
+            stmt.setString(2, nmFuncionario);
+            stmt.setDate(3, java.sql.Date.valueOf(dtNascimento));
+            stmt.setDouble(4, salario);
+            stmt.setString(5, depart);
+            stmt.setString(6, cargo);
+
+            stmt.executeUpdate();
+            System.out.println("Funcionário inserido com sucesso no banco de dados.");
+        } catch (SQLException e) {
+            System.out.println("Erro ao tentar inserir funcionário no banco de dados: " + e.getMessage());
+        }
+    }
+
+
+    //CONSULTAR FUNCIONÁRIOS.
+    public static List<Funcionario> consultarFuncionarios() {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        String sql = "SELECT * FROM funcionarios";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/seu_banco_de_dados", "seu_usuario", "sua_senha");
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                int cdFuncionario = rs.getInt("cdFuncionario");
+                String nmFuncionario = rs.getString("nmFuncionario");
+                LocalDate dtNascimento = rs.getDate("dtNascimento").toLocalDate();
+                double salario = rs.getDouble("salario");
+                String depart = rs.getString("depart");
+                String cargo = rs.getString("cargo");
+
+                Funcionario funcionario = new Funcionario(cdFuncionario, nmFuncionario, dtNascimento, salario, depart, cargo);
+                funcionarios.add(funcionario);
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao consultar funcionários: " + e.getMessage());
+        }
+
+        return funcionarios;
+    }
+
+
+
+
+    public static void imprimirFuncionarios(List<Funcionario> funcionarios) {
+        for (Funcionario funcionario : funcionarios) {
+            System.out.println("Código: " + funcionario.getCdFuncionario());
+            System.out.println("Nome: " + funcionario.getNmFuncionario());
+            System.out.println("Departamento: " + funcionario.getDepart());
+            System.out.println("Salário: " + funcionario.getSalario());
+            System.out.println("Data de Nascimento: " + funcionario.getDtNascimento());
+            System.out.println("--------------------------");
+        }
+    }
+    
+
+    //GETTERS E SETTERS 
+    public int getCdFuncionario() {
+        return cdFuncionario;
+    }
+    public void setCdFuncionario(int cdFuncionario) {
+        this.cdFuncionario = cdFuncionario;
+    }
+    public String getNmFuncionario() {
+        return nmFuncionario;
+    }
+    public void setNmFuncionario(String nmFuncionario) {
+        this.nmFuncionario = nmFuncionario;
+    }
+    public LocalDate getDtNascimento() {
+        return dtNascimento;
+    }
+    public void setDtNascimento(LocalDate dtNascimento) {
+
+        
+        this.dtNascimento = dtNascimento;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public String getDepart() {
+        return depart;
+    }
+    public void setDepart(String depart) {
+        this.depart = depart;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+}
---
+++ src/main/java/com/example/ModeloConexaoJava.java
@@ -0,0 +1,97 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+//import javax.naming.spi.DirStateFactory.Result;
+
+public class ModeloConexaoJava 
+{
+
+    static void consultaDados (Statement stmConsulta){
+        //Statemente é o modo pra colocar informação dentro do banco de dados 
+
+        //nessa query vai armazenar o comando sql pra ser executado pelo banco de dados 
+        String querry = "SELECT INFORMAÇÕES, DATABELA FROM NOME_TABELA";
+        try {
+            //stmConsulta.executeQuery(querry) vai executar o comando e vai armazernar no result
+            //esse result é comando do sql que vai passando pela tabela e fornecendo o resultado dos 
+            //objetos
+
+        ResultSet result = stmConsulta.executeQuery(querry);
+            //o while vai fazer o result andar pela tabela imprimindo os coisas
+            while (result.next()) {
+            System.out.println("id Funcioário:" + result.getInt(0)+"nome: "+result.getString(0));
+    
+}
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+ 
+
+    }
+
+    static void colocaDados (Statement stm) {
+
+        //essa é a syntax que o sql entende, a gente coloca ela dentro de uma váriavel 
+        //assim é como se aquela variavel guardasse aquela "carta"
+                //onde tem nome e Pablo é o nome da variavel na tabela e a alteração 
+        String inseriSql = "insert into usuario (nome) values (Pablo)";
+
+        try {
+            //esse . executeUpdate é pra poder fazer a alteração 
+            stm.executeUpdate(inseriSql);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            // TODO: handle exception
+        }
+
+
+    }
+   
+
+
+    public static void main( String[] args )
+    {
+
+        // aqui já tem o modelo q eu vou usar pra conectar 
+      String urlDB = "jdbc:postgresql://localhost:5432/postgres";
+      String userDB = "postgres";
+      String passwordDB = "7412369";
+
+
+        try {
+            // essa linha vai ser responsavel por "logar"
+            Connection conex = DriverManager.getConnection(urlDB, userDB, passwordDB);
+
+            if (conex !=null ) {
+                //abaixo vai nos mostrar que a conexão foi estabelecida
+
+                System.out.println("prova dos 9 pra logado!!");
+                //abaixo foi criado o Statement pra poder ser usado o metódo consultaDados.
+                //esse metodo consiste em inserir uma query no banco de dado que vai ler num while
+                //todos os objetos daquela tabela  
+                Statement stm = conex.createStatement();
+                colocaDados(stm);// <-chamada do metodo de colocar Dados
+                consultaDados(stm);// <-chamada do metodo de consultar Dados com um loop While
+            
+            } else{
+                System.out.println("conecção falhou :()");
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+       }
+     
+        
+   }
+
+
+}
